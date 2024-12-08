c6687247d02bca1a8c760bda29f7ba988577c748
Owner <aryanjairath7@gmail.com>
2024-12-08 15:30:58 -0500

    Added work content

--- src/components/work/work.jsx
+++ src/components/work/work.jsx
@@ -1,7 +1,19 @@
+import experience from "./workdata"
+import './work.css'
 const Work = () => {
     return (
         <section id = "work">
             <h1>Volunteer and Work Experience</h1>
+            {experience.map(item => (
+                    <div className="work-container">
+                        <div className="work-item">
+                            <h1 className="title">{item.title}</h1>
+                            <h2 className = "description">{item.description}</h2>
+                        </div>
+                    </div>
+                )
+            )   
+            }   
         </section>
     )
 }
